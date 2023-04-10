9ae0aed1642e853ecad0bd267b72e317f4a09525
Tammy George <georgetprecious@gmail.com>
2023-04-10 09:09:53 +0100

    Add files via upload

---
+++ myportfolio/assets/js/script-nav.js
@@ -0,0 +1,32 @@
+
+// Toggle the .pa-fixed-header class when the user 
+// scroll 100px 
+
+window.onscroll = () => {scrollNavbar()};
+
+scrollNavbar = () => {
+    // Target elements
+    const navBar = document.getElementById("navBar");
+    const links = document.querySelectorAll("#navBar a,i");
+   
+
+  if (document.documentElement.scrollTop > 100) {
+    navBar.classList.add('nav-colored');
+
+    // Change the color of links on scroll
+    for (let i = 0; i < links.length; i++) {
+        const element = links[i];
+        element.classList.add('text-black');
+    }
+
+  } else {
+    navBar.classList.remove("nav-colored");
+    
+    // Change the color of links back to default
+    for (let i = 0; i < links.length; i++) {
+        const element = links[i];
+        element.classList.remove('text-black');
+    }
+  }
+}
+
